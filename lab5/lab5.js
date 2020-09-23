729391a1812bd7fd2a48f8644feaf753fbbd747a
sala8552 <70294977+sala8552@users.noreply.github.com>
2020-09-22 23:00:51 -0700

    Add files via upload

---
+++ lab5/lab5.js
@@ -0,0 +1,22 @@
+ //Author: Elias Salas <elsalas@csumb.edu>
+ //Created: September 16
+ //License: Public Domain
+
+
+console.log("lab 5")
+
+// Variables defined
+var make = "chevy";
+var model = "colorado";
+var color = "grey";
+var year = 2019;
+
+// calculate
+age = 2019 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
